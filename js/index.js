b1cce74cd0f22d72295d1c3ae12e55612ed466d5
prasetion <prasetion@gmail.com>
2021-06-01 11:31:51 +0700

    index js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,34 @@
-let foods = [
-    "1",
-    "2",
-    "3"
+// let foods = [
+//     "1",
+//     "2",
+//     "3"
+// ]
+
+class RockPaperScissors {
+    constructor(name) {
+        this.name = name
+    }
+
+    getName() {
+        console.log(`i choose ${this.name}`)
+    }
+
+    getResult() {
+        console.log("the winner is ")
+    }
+}
+
+// suit item
+let suitItems = [
+    "Batu",
+    "Gunting",
+    "Kertas"
 ]
 
+// assign button suit
+let buttonSuit = document.getElementById("changeButton").onclick = randomResult
+
+// get random result
+function randomResult() {
+
+}
